c08dbaba5d2ea77d54da7bb13ac0cf9f6526c274
luismvargasg <luismvargasg@gmail.com>
2024-04-01 11:53:08 -0500

    fixing #526 - now the component uses the ClienApi call function

--- app/components/ClientSide/DocumentModal/DocumentModal.js
+++ app/components/ClientSide/DocumentModal/DocumentModal.js
@@ -1,15 +1,15 @@
 "use client";
 
-/* Hooks */
-import { useState, useEffect } from "react";
-
 /* Components */
 import AuthorsHorizontalList from "../AuthorsHorizontalList/AuthorsHorizontalList";
+import Error from "@/app/app/error";
 import Loading from "@/app/app/loading";
 
 /* lib */
 import RenderedExternalIDs from "@/lib/RenderedExternalIDs";
 import RenderedExternalURLs from "@/lib/RenderedExternalURLs";
+import URLBuilder from "@/lib/URLBuilder";
+import { APIRequest } from "@/lib/clientAPI";
 
 /* UI Library Components */
 import { Divider, Descriptions, Typography } from "antd";
@@ -18,102 +18,93 @@ import { Divider, Descriptions, Typography } from "antd";
 const { Text, Paragraph } = Typography;
 
 /**
- * Renders a modal with information about a document.
- * @param {string} props.documentID - The ID of the document to fetch and display information for.
- * @returns {JSX.Element} - The DocumentModal component.
+ * DocumentModal is a client-side function component that fetches and displays information about a document.
+ *
+ * @param {string} documentID - The ID of the document to fetch and display information for.
+ *
+ * @returns {JSX.Element} If the API request is loading, a Loading component is displayed.
+ * If the API request has an error, an Error component is displayed.
+ * Otherwise, a div containing the document's authors, abstract, and other details is displayed.
  */
 export default function DocumentModal({ documentID }) {
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
+  const URL = URLBuilder(`/app/work/${documentID}`, { section: "info" });
+  const [state] = APIRequest(URL);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(
-        `http://apis.impactu.colav.co:8010/app/work/${documentID}?section=info`
-      );
-      const { data } = await res.json();
-      setData(data);
-      setLoading(false);
-    };
-    console.log(
-      `http://apis.impactu.colav.co:8010/app/work/${documentID}?section=info`
+  if (state.isError) {
+    return <Error />;
+  } else if (state.isLoading) {
+    return <Loading />;
+  } else {
+    const {
+      authors,
+      abstract,
+      source,
+      language,
+      volume,
+      year_published,
+      issue,
+      citations_count,
+      external_ids,
+      external_urls,
+    } = state.data.data;
+
+    return (
+      <div>
+        <Text strong>Autores: </Text>
+        {<AuthorsHorizontalList authors={authors} />}
+        <Divider style={{ margin: "15px 0" }} />
+        <Text strong>Abstract:</Text>
+        <Paragraph>{abstract || "No disponible"}</Paragraph>
+        <Divider style={{ margin: "15px 0" }} />
+        <Descriptions bordered column={{ lg: 3, md: 2, sm: 2, xs: 1 }}>
+          <Descriptions.Item label="Revista:">
+            {source?.name && source?.serials?.openalex ? (
+              <div>
+                {source.name}
+                <br />
+                <a
+                  href={source.serials.openalex}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {source.serials.openalex}
+                </a>
+              </div>
+            ) : (
+              "No disponible"
+            )}
+          </Descriptions.Item>
+          <Descriptions.Item label="Idioma:">
+            {language || "No disponible"}
+          </Descriptions.Item>
+          <Descriptions.Item label="Volumen:">
+            {volume || "No disponible"}
+          </Descriptions.Item>
+          <Descriptions.Item label="Publicado:">
+            {year_published || "No disponible"}
+          </Descriptions.Item>
+          <Descriptions.Item label="Issue:">
+            {issue || "No disponible"}
+          </Descriptions.Item>
+          <Descriptions.Item label="pISSN:">
+            <Text>{source?.serials?.pissn || "No disponible"}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label="ISSN:">
+            <Text>{source?.serials?.issn || "No disponible"}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label="Scimago:">
+            <Text>{source?.serials?.scimago || "No disponible"}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label="Scienti:">
+            <Text>{source?.serials?.scienti || "No disponible"}</Text>
+          </Descriptions.Item>
+          <Descriptions.Item label="Citaciones:">
+            {citations_count || "No disponible"}
+          </Descriptions.Item>
+          {RenderedExternalIDs(external_ids)}
+          {RenderedExternalURLs(external_urls)}
+        </Descriptions>
+      </div>
     );
-    fetchData();
-  }, [documentID]);
-
-  if (isLoading) return <Loading />;
-  if (!data) return <p>Información no disponible.</p>;
-
-  /* Destructure the data object for easier access to its properties */
-  const {
-    authors,
-    abstract,
-    source,
-    language,
-    volume,
-    year_published,
-    issue,
-    citations_count,
-    external_ids,
-    external_urls,
-  } = data;
-
-  return (
-    <div>
-      <Text strong>Autores: </Text>
-      {<AuthorsHorizontalList authors={authors} />}
-      <Divider style={{ margin: "15px 0" }} />
-      <Text strong>Abstract:</Text>
-      <Paragraph>{abstract || "No disponible"}</Paragraph>
-      <Divider style={{ margin: "15px 0" }} />
-      <Descriptions bordered column={{ lg: 3, md: 2, sm: 2, xs: 1 }}>
-        <Descriptions.Item label="Revista:">
-          {source?.name && source?.serials?.openalex ? (
-            <div>
-              {source.name}
-              <br />
-              <a
-                href={source.serials.openalex}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {source.serials.openalex}
-              </a>
-            </div>
-          ) : (
-            "No disponible"
-          )}
-        </Descriptions.Item>
-        <Descriptions.Item label="Idioma:">
-          {language || "No disponible"}
-        </Descriptions.Item>
-        <Descriptions.Item label="Volumen:">
-          {volume || "No disponible"}
-        </Descriptions.Item>
-        <Descriptions.Item label="Publicado:">
-          {year_published || "No disponible"}
-        </Descriptions.Item>
-        <Descriptions.Item label="Issue:">
-          {issue || "No disponible"}
-        </Descriptions.Item>
-        <Descriptions.Item label="pISSN:">
-          <Text>{source?.serials?.pissn || "No disponible"}</Text>
-        </Descriptions.Item>
-        <Descriptions.Item label="ISSN:">
-          <Text>{source?.serials?.issn || "No disponible"}</Text>
-        </Descriptions.Item>
-        <Descriptions.Item label="Scimago:">
-          <Text>{source?.serials?.scimago || "No disponible"}</Text>
-        </Descriptions.Item>
-        <Descriptions.Item label="Scienti:">
-          <Text>{source?.serials?.scienti || "No disponible"}</Text>
-        </Descriptions.Item>
-        <Descriptions.Item label="Citaciones:">
-          {citations_count || "No disponible"}
-        </Descriptions.Item>
-        {RenderedExternalIDs(external_ids)}
-        {RenderedExternalURLs(external_urls)}
-      </Descriptions>
-    </div>
-  );
+  }
 }
