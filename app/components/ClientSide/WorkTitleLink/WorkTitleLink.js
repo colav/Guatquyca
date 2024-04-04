d3215138681a4557d00475ccd7aa39f7515121ed
luismvargasg <luismvargasg@gmail.com>
2024-04-04 09:28:31 -0500

    Fix #530 Now the modal is imported from App Component - AntD

--- app/components/ClientSide/WorkTitleLink/WorkTitleLink.js
+++ app/components/ClientSide/WorkTitleLink/WorkTitleLink.js
@@ -5,51 +5,53 @@ import DocumentModal from "../DocumentModal/DocumentModal";
 import OpenAccessStatus from "../../ServerSide/OpenAccessStatus/OpenAccessStatus";
 
 /* UI Library Components */
-import { Modal } from "antd";
+import { App } from "antd";
 
 /**
- * WorkTitleLink is a component that displays a link with the work title.
- * When the link is clicked, it opens a modal with the document information.
+ * WorkTitleLink is a client-side function component that displays a link with the title of a work.
+ * When the link is clicked, a modal is shown with information about the work.
  *
  * @param {string} workTitle - The title of the work.
  * @param {string} workID - The ID of the work.
  * @param {string} openAccessStatus - The open access status of the work.
- * @returns {JSX.Element} - The Title of a work as a Link that triggers a Modal component.
+ *
+ * @returns {JSX.Element} An App component containing a TitleModal component.
+ * The TitleModal component is a link with the title of the work.
+ * When the link is clicked, a modal is shown with information about the work.
  */
 export default function WorkTitleLink({ workTitle, workID, openAccessStatus }) {
-  /**
-   * docInfo is a function that opens a modal with the document information.
-   *
-   * @param {string} title - The title of the work.
-   * @param {string} id - The ID of the work.
-   * @param {string} status - The open access status of the work.
-   */
-  const docInfo = (title, id, status) => {
-    Modal.warning({
-      width: "1200px",
-      title: [
-        title,
-        " ",
-        status && <OpenAccessStatus status={status} key="0" />,
-      ],
-      icon: null,
-      okText: "Cerrar",
-      content: <DocumentModal documentID={id} />,
-      destroyOnClose: true,
-      maskClosable: true,
-      onOk() {},
-    });
-  };
+  const TitleModal = () => {
+    const { modal } = App.useApp();
+    const showModal = (title, id, status) => {
+      modal.warning({
+        width: "1200px",
+        title: [
+          title,
+          " ",
+          status && <OpenAccessStatus status={status} key="0" />,
+        ],
+        icon: null,
+        okText: "Cerrar",
+        content: <DocumentModal documentID={id} />,
+        destroyOnClose: true,
+        maskClosable: true,
+        onOk() {},
+      });
+    };
 
-  return (
-    <>
+    return (
       <a
         type="link"
-        onClick={() => docInfo(workTitle, workID, openAccessStatus)}
+        onClick={() => showModal(workTitle, workID, openAccessStatus)}
       >
         {workTitle}
       </a>
-      {openAccessStatus ? <OpenAccessStatus status={openAccessStatus} /> : ""}
-    </>
+    );
+  };
+
+  return (
+    <App>
+      <TitleModal />
+    </App>
   );
 }
