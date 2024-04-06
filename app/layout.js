d0389f6a4b56de53a9cac78e4169e6b00bc3ed66
luismvargasg <luismvargasg@gmail.com>
2024-04-06 09:32:02 -0500

    Add Google Fonts link to RootLayout component in layout.js, using a new global font.

--- app/layout.js
+++ app/layout.js
@@ -27,12 +27,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>
         <ConfigProvider
           theme={{
             token: {
               colorPrimary: "#F9B250",
               borderRadius: 6,
+              fontFamily: "Montserrat",
             },
           }}
         >
