d4e6544d325b7fcf3d46f5e01fa549da0eebd7c7
luismvargasg <luismvargasg@gmail.com>
2024-08-01 16:48:08 -0500

    Temporarily disabling choropleth maps colav/impactu#229

--- app/affiliation/[entity]/[ID]/research/products/page.js
+++ app/affiliation/[entity]/[ID]/research/products/page.js
@@ -27,15 +27,15 @@ export default function ProductsOnEntityPage({ params }) {
       {params.entity === "institution" ? (
         <div>
           <Row gutter={15} style={{ marginBottom: "15px" }}>
-            <Col xs={24} sm={24} md={8}>
+            <Col xs={24} sm={24} md={12}>
               <DistributionChartsHandler entity={params.entity} />
             </Col>
-            <Col xs={24} sm={24} md={8}>
+            <Col xs={24} sm={24} md={12}>
               <PercentageChartsHandler entity={params.entity} />
             </Col>
-            <Col xs={24} sm={24} md={8}>
+            {/* <Col xs={24} sm={24} md={8}>
               <MapChartsHandler entity={params.entity} />
-            </Col>
+            </Col> */}
             {/* <Col xs={24} sm={24} md={12}>
               <GraphChartsHandler entity={params.entity} />
             </Col> */}
@@ -44,15 +44,15 @@ export default function ProductsOnEntityPage({ params }) {
       ) : (
         <div>
           <Row gutter={[15, 15]} style={{ marginBottom: "15px" }}>
-            <Col xs={24} sm={24} md={8}>
+            <Col xs={24} sm={24} md={12}>
               <DistributionChartsHandler entity={params.entity} />
             </Col>
-            <Col xs={24} sm={24} md={8}>
+            <Col xs={24} sm={24} md={12}>
               <PercentageChartsHandler entity={params.entity} />
             </Col>
-            <Col xs={24} sm={24} md={8}>
+            {/* <Col xs={24} sm={24} md={8}>
               <MapChartsHandler entity={params.entity} />
-            </Col>
+            </Col> */}
           </Row>
         </div>
       )}
