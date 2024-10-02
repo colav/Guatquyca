2be6c7ba60670a2da75131d99d9d3c6416e7746b
luismvargasg <luismvargasg@gmail.com>
2024-10-02 09:49:21 -0500

    refactor: Add EmptyAffiliations component to display a message when there are no affiliations

--- app/affiliation/[entity]/[ID]/affiliations/page.js
+++ app/affiliation/[entity]/[ID]/affiliations/page.js
@@ -1,5 +1,6 @@
 /* Components */
 import ClientLogger from "@/lib/Utils/clientLogger";
+import EmptyAffiliations from "@/app/components/ClientSide/EmptyAffiliations/EmptyAffiliations";
 import ListCard from "@/app/components/ClientSide/ListCard/ListCard";
 import TopMenu from "@/app/components/ClientSide/TopMenu/TopMenu";
 
@@ -13,29 +14,42 @@ import { Col, Row } from "antd";
 import { SINGULAR } from "@/lib/constants";
 
 /**
- * Page is an asynchronous function component that fetches data based on provided parameters and displays it using
- * the TopMenu and ListCard components.
+ * Page is an asynchronous function component that fetches data based on provided parameters
+ * and displays it using the TopMenu and ListCard components. If no data is found, it displays
+ * the EmptyAffiliations component.
  *
+ * @component
  * @param {Object} params - The parameters used to fetch data.
- * @returns {JSX.Element} A fragment that contains a TopMenu component and a Row component that maps over the
- * fetched data to display a ListCard component for each item.
+ * @param {string} params.entity - The entity type to fetch data for.
+ * @param {string} params.ID - The ID of the entity to fetch data for.
+ * @returns {JSX.Element} A fragment that contains a TopMenu component and a Row component that
+ * maps over the fetched data to display a ListCard component for each item, or an EmptyAffiliations
+ * component if no data is found.
+ * @example
+ * return (
+ *   <Page params={{ entity: "institution", ID: "12345" }} />
+ * )
  */
-export default async function Page({ params }) {
+export default async function AffiliationsPage({ params }) {
   const URL = `/app/affiliation/${params.entity}/${params.ID}/affiliations`;
   const { data, fullUrl } = await getData(URL);
-  // Filter out any keys that have a falsy value
-  const keys = Object.keys(data).filter((key) => data[key]);
+
+  const keys = Object.keys(data).filter((key) => data[key].length > 0);
 
   return (
     <>
       <ClientLogger url={fullUrl} />
       <TopMenu currentTab="affiliations" />
-      <Row style={{ marginTop: "15px" }} gutter={15}>
-        {keys.map((item) => (
-          <Col xs={24} md={24 / keys.length} key={item}>
-            <ListCard type={SINGULAR[item]} list={data[item]} />
-          </Col>
-        ))}
+      <Row style={{ marginTop: "15px" }} gutter={15} justify="center">
+        {keys.length === 0 ? (
+          <EmptyAffiliations />
+        ) : (
+          keys.map((item) => (
+            <Col xs={24} md={24 / keys.length} key={item}>
+              <ListCard type={SINGULAR[item]} list={data[item]} />
+            </Col>
+          ))
+        )}
       </Row>
     </>
   );
