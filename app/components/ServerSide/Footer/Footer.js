bc7eafec7e6b73bd4be67b1c3de9fa1780c09eca
luismvargasg <luismvargasg@gmail.com>
2024-10-16 16:00:02 -0500

    Refactor: Update version number in Footer component and package.json

--- app/components/ServerSide/Footer/Footer.js
+++ app/components/ServerSide/Footer/Footer.js
@@ -136,11 +136,11 @@ export default function Foot() {
           <div style={{ marginBottom: "10px" }}>
             <b>Información:</b>
           </div>
-          ImpactU Versión 3.5.0-beta
+          ImpactU Versión 3.5.1-beta
           <br />
           Última actualización:
           <br />
-          Interfaz de Usuario: 02/10/2024
+          Interfaz de Usuario: 16/10/2024
           <br />
           Base de Datos: 02/10/2024
           <br />
