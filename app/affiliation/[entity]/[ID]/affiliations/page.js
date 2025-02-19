c3dbffef56abcba7eb26b09be0ce484e5ba49b3b
luismvargasg <luismvargasg@gmail.com>
2025-02-18 21:28:35 -0500

    fix: improve key filtering logic in AffiliationsPage for non-empty arrays

--- app/affiliation/[entity]/[ID]/affiliations/page.js
+++ app/affiliation/[entity]/[ID]/affiliations/page.js
@@ -27,21 +27,17 @@ import { SINGULAR } from "@/lib/constants";
  * component if no data is found.
  * @example
  * return (
- *   <Page params={{ entity: "institution", ID: "12345" }} />
+ *   <AffiliationsPage params={{ entity: "institution", ID: "12345" }} />
  * )
  */
 export default async function AffiliationsPage({ params }) {
   const URL = `/app/affiliation/${params.entity}/${params.ID}/affiliations`;
   const { data, fullUrl } = await getData(URL);
 
-  const participants = ["00jb9vg53", "03bp5hc83", "02xtwpk10", "05tkb8v92"];
-  let keys = Object.keys(data).filter((key) => data[key]);
-  if (!participants.includes(params.ID)) {
-    keys = keys.filter((key) => key !== "faculties" && key !== "departments");
-  }
-  if (params.ID === "05tkb8v92") {
-    keys = keys.filter((key) => key !== "departments");
-  }
+  // Filter keys to include only those with non-empty arrays
+  let keys = Object.keys(data).filter(
+    (key) => Array.isArray(data[key]) && data[key].length > 0
+  );
 
   return (
     <>
