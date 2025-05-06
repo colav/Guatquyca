5f041454974002007f602c21475ca9aca088f3ff
luismvargasg <luismvargasg@gmail.com>
2025-05-06 12:29:36 -0500

    feat: enhance documentation for RankingTag component with detailed comments

--- app/components/ServerSide/RankingTag/RankingTag.js
+++ app/components/ServerSide/RankingTag/RankingTag.js
@@ -4,13 +4,23 @@ import styles from "./styles.module.css";
 /* UI Library Components */
 import { Tag, Tooltip } from "antd";
 
+/**
+ * RankingTag Component
+ *
+ * This component displays a ranking tag based on a provided ranking list.
+ * It prioritizes rankings from the source "scienti" first, and if not found,
+ * it selects rankings from the source "minciencias" only if the item does not
+ * contain the "level" key.
+ *
+ * @component
+ * @param {Object[]} ranking - The list of ranking objects.
+ * @returns {JSX.Element|null} A tooltip-wrapped tag displaying the rank, or null if no valid rank is found.
+ */
 export default function RankingTag({ ranking }) {
   if (!ranking || ranking.length === 0) {
     return null;
   }
 
-  // Filter the ranking list based on the hierarchy of sources.
-  // source === "scienti" is the highest priority, followed by "minciencias" without a level.
   const rank = ranking.find(
     (item) =>
       item.source === "scienti" ||
