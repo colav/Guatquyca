0e2857f4630d6f12018b7506a7c8cb1673f2c135
luismvargasg <luismvargasg@gmail.com>
2025-10-09 22:20:13 -0500

    refactor: add delay in runSequentially function to avoid API rate limit fails when testing metrics

--- tests/e2e/authors.spec.js
+++ tests/e2e/authors.spec.js
@@ -296,6 +296,7 @@ test.describe("Testing Authors entity", () => {
     async function runSequentially(plotlist) {
       for (const item of plotlist) {
         await fetchAndMeasure(item);
+        await new Promise((resolve) => setTimeout(resolve, 100));
       }
     }
 
--- tests/e2e/institutions.spec.js
+++ tests/e2e/institutions.spec.js
@@ -274,6 +274,7 @@ test.describe("Testing Institutions entity", () => {
     async function runSequentially(plotlist) {
       for (const item of plotlist) {
         await fetchAndMeasure(item);
+        await new Promise((resolve) => setTimeout(resolve, 100));
       }
     }
 
