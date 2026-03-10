ebe7b4b99089eef6526a982feb63c16e9df53977
luismvargasg <luismvargasg@gmail.com>
2026-03-09 22:40:33 -0500

    fix: update MapChart style from 'light' to 'blank' to avoid some external api error

--- app/components/ClientSide/Charts/MapCharts/MapChart.js
+++ app/components/ClientSide/Charts/MapCharts/MapChart.js
@@ -24,7 +24,7 @@ export default function MapChart({ data }) {
     () => ({
       map: {
         type: "mapbox",
-        style: "light",
+        style: "blank",
       },
       source: {
         data: data,
